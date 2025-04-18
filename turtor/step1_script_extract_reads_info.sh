8b17d43b8132c994ea4db8ecbad18fa0a295dba3
xiaokanglong <126579107+jue-hen@users.noreply.github.com>
2025-04-18 21:34:29 +0800

    Add files via upload

---
+++ turtor/step1_script_extract_reads_info.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+###if you have some double-end sequencing reads file, for example
+###sample1_r1.fq.gz    sample2_r2.fq.gz
+###sample2_r1.fq.gz    sample2_r2.fq.gz
+###sample3_r1.fq.gz    sample2_r2.fq.gz
+##etc...
+
+##you can use this script to extract ALT/REF info from sequencing reads 
+for i in *
+do
+    j=$(basename $i | cut -d "_" -f 1 | uniq)
+    ../fq2depth ../site.probe.avx2 ./${j}.depth ${j}*.gz
+done;
+    
